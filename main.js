dda6bb85595850cb16b6e8685a9535d9f9d5eff6
Kenny Liao <yenming891109@berkeley.edu>
2021-07-22 17:51:55 +0800

    add: regex for google sheet columns

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ const sheet_id = localStorage.getItem("sheet_id")
 const add_button = document.getElementById("add_button")
 
 //columns to input value
-var job; 
-var title;	
+var job_title	
 var company;
 var	location;	
 var date_posted;	
@@ -13,17 +12,30 @@ var status;
 var target_url;
 
 //functions
-function html_parser () {
-    console.log(this.responseText);
+function parser (title) {
+    //Get company
+    let reg = "[A-Za-z0-9]+ hiring "
+    let matcher = title.match(reg)[0]
+    company = matcher.split(" ")[0]
+    //Get Job title
+    reg = "(?<=hiring )(.+)(?= in )"
+    job_title = title.match(reg)[0]
+    //Get location
+    reg = "(?<= in )[^\|]+(?= | LinkedIn )"
+    location = title.match(reg)[0]
+    console.log(location, company, job_title)
 }
-
+//https://www.linkedin.com/jobs/view/2626542900/
 function httpGetAsync(theUrl)
 {
     let xmlHttp = new XMLHttpRequest();
-    xmlHttp.addEventListener("load", html_parser);
+    xmlHttp.onload = function() {
+        console.log(this.responseXML.title)
+        parser(this.responseXML.title)
+      }
     xmlHttp.open("GET", theUrl);
+    xmlHttp.responseType = "document";
     xmlHttp.send();
-    return this.responseText
 }
 
 add_button.addEventListener("click", function() {
